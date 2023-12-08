28d4c30154840a87ef0471b76fdaa58b5532dc05
Nathan <nathan.hilde1@gmail.com>
2023-12-08 01:02:40 -0800

    added display user's name code to thanks.js and removed main.js from thanks.html to correct main.js errors on thanks page

--- scripts/thanks.js
+++ scripts/thanks.js
@@ -3,3 +3,18 @@ function goToLibrary(){
     window.location.href = "postsLibrary.html"
     
 }
+
+function getNameFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            console.log(user.uid); 
+            console.log(user.displayName);
+            userName = user.displayName;
+            document.getElementById("name-goes-here").innerText = userName; 
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+getNameFromAuth();
